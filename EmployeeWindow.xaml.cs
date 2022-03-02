42e6c0f7d0f5436ebf790ec0f39f37df8b0f240a
Jovo Savic <jovo.savic00@gmail.com>
2022-03-02 15:00:15 +0100

    Add, Edit, Delete a member implemented

--- EmployeeWindow.xaml.cs
+++ EmployeeWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace Teretana
 {
-    /// <summary>
-    /// Interaction logic for EmployeeWindow.xaml
-    /// </summary>
+    //TODO: prilikom dodavanja nove osobe, dodati ga i kao novog clana
     public partial class EmployeeWindow : Window
     {
         MySqlConnection teretanaDB = new MySqlConnection(Config.dbConfigString);
@@ -69,47 +67,61 @@ namespace Teretana
         }
         private void SetTrainings(int id)
         {
-            string querry = $"select * from trening where Clan_IdOsoba='{id}'";
-            teretanaDB.Open();
-            MySqlCommand cmd = teretanaDB.CreateCommand();
-            cmd.CommandText = querry;
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            List<Training> trainingList = new List<Training>();
-
-            while (reader.Read())
+            try
             {
-                trainingList.Add(new Training(reader.GetDateTime(0), reader.GetDateTime(1)));
+
+
+                string querry = $"select * from trening where Clan_IdOsoba='{id}'";
+                teretanaDB.Open();
+                MySqlCommand cmd = teretanaDB.CreateCommand();
+                cmd.CommandText = querry;
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                List<Training> trainingList = new List<Training>();
+
+                while (reader.Read())
+                {
+                    trainingList.Add(new Training(reader.GetDateTime(0), reader.GetDateTime(1)));
+                }
+                trainingsListView.ItemsSource = trainingList;
+                teretanaDB.Close();
+            }catch (Exception ex)
+            {
+                teretanaDB.Close();
             }
-            trainingsListView.ItemsSource = trainingList;
-            teretanaDB.Close();
         }
         private void SetMemberShip(int id)
         {
+            try
+            {
+                teretanaDB.Open();
+                string querry = $"select DatumUplate, TrajanjeClanarine from clanarina join clan on Clan.Clan_IdOsoba=Clanarina.Clan_IdOsoba where Clan.Clan_IdOsoba={id}";
+                MySqlCommand cmd = teretanaDB.CreateCommand();
+                cmd.CommandText = querry;
 
-            teretanaDB.Open();
-            string querry = $"select DatumUplate, TrajanjeClanarine from clanarina join clan on Clan.Clan_IdOsoba=Clanarina.Clan_IdOsoba where Clan.Clan_IdOsoba={id}";
-            MySqlCommand cmd = teretanaDB.CreateCommand();
-            cmd.CommandText= querry;
+                MySqlDataReader reader = cmd.ExecuteReader();
 
-            MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
 
-            reader.Read();
+                DateTime paidAt = reader.GetDateTime(0);
+                string membershipDuration = reader.GetString(1);
+                DateTime validUntil = paidAt.AddDays(int.Parse(membershipDuration));
+                TimeSpan daysLeft = validUntil.Subtract(DateTime.Now);
+                paidAtLbl.Content = paidAt.ToShortDateString();
+                validUntilLbl.Content = validUntil.ToShortDateString();
+                daysLeftLbl.Content = daysLeft.Days.ToString();
 
-            DateTime paidAt = reader.GetDateTime(0);
-            string membershipDuration = reader.GetString(1);
-            DateTime validUntil = paidAt.AddDays(int.Parse(membershipDuration));
-            TimeSpan daysLeft = validUntil.Subtract(DateTime.Now);
-            paidAtLbl.Content = paidAt.ToShortDateString();
-            validUntilLbl.Content = validUntil.ToShortDateString();
-            daysLeftLbl.Content = daysLeft.Days.ToString();
-
-            teretanaDB.Close();
+                teretanaDB.Close();
+            }catch (Exception ex)
+            {
+                teretanaDB.Close();
+                ClearMembershipFields();
+            }
         }
         private void SetMemberInfo(ListView sender)
         {
             ListView? listView = sender;
-            int idOsoba = ((BasicMemberInfo)listView.Items.CurrentItem).Id;
+            int idOsoba = ((BasicMemberInfo)listView.SelectedItem).Id;
 
             teretanaDB.Open();
             string querry = $"select * from osoba where IdOsoba={idOsoba}";
@@ -178,28 +190,53 @@ namespace Teretana
             
         private void InsertNewMemberIntoDb(BasicMemberInfo member)
         {
-            string date = String.Format("{0:yyyy/MM/dd}", member.DateTime);
-
-            string querry = $"insert into osoba (ime, prezime, datumrodjenja, jmbg, email,opstina_postanskibroj) " +
-                $"values ('{member.Name}','{member.Surname}','{date}','{member.JMBG}','{member.Email}','{member.PostanskiBroj}')";
-            teretanaDB.Open();
-            MySqlCommand cmd = teretanaDB.CreateCommand();
-            cmd.CommandText = querry;
-            if (cmd.ExecuteNonQuery() == -1)
+            try
             {
-                MessageBox.Show("Error Inserting new member");
+                string date = String.Format("{0:yyyy/MM/dd}", member.DateTime);
+
+                string querry = $"insert into osoba (ime, prezime, datumrodjenja, jmbg, email,opstina_postanskibroj) " +
+                    $"values ('{member.Name}','{member.Surname}','{date}','{member.JMBG}','{member.Email}','{member.PostanskiBroj}')" +
+                    $"ON DUPLICATE KEY UPDATE `ime`='{member.Name}', `prezime`='{member.Surname}', `datumrodjenja`='{date}', `jmbg`='{member.JMBG}', `email`='{member.Email}'," +
+                    $"`opstina_postanskibroj`= '{member.PostanskiBroj}'";
+                teretanaDB.Open();
+                MySqlCommand cmd = teretanaDB.CreateCommand();
+                cmd.CommandText = querry;
+                if (cmd.ExecuteNonQuery() == -1)
+                {
+                    MessageBox.Show("Error Inserting new member");
+                }
+                long newMemberId = cmd.LastInsertedId;
+                string querry2 = $"insert into clan (clan_idosoba,brojdolazaka) values ('{newMemberId}', '{0}') ON DUPLICATE KEY UPDATE " +
+                    $"`clan_idosoba`='{newMemberId}'";
+                cmd.CommandText = querry2;
+                cmd.ExecuteNonQuery();
+                teretanaDB.Close();
             }
-            teretanaDB.Close();
+            catch(MySqlException ex)
+            {
+                if(!ex.Message.Contains("Cannot add or update a child row: a foreign key constraint fails"))
+                {
+                    MessageBox.Show("Unable to add a new member.");
+                }
+                teretanaDB.Close();
+            }
         }
         BasicMemberInfo GetNewMemberInfo()
         {
             BasicMemberInfo newMember = new BasicMemberInfo();
-            newMember.Name = nameTextBox.Text;
-            newMember.Surname = surnameTextBox.Text;
-            newMember.JMBG = jmbgTextBox.Text;
-            newMember.Email = emailTextBox.Text;
-            newMember.DateTime = datePicker.SelectedDate.Value.Date;
-            newMember.PostanskiBroj = GetPostalCode(citiesComboBox.SelectedItem.ToString());
+            try
+            {
+                newMember.Name = nameTextBox.Text;
+                newMember.Surname = surnameTextBox.Text;
+                newMember.JMBG = jmbgTextBox.Text;
+                newMember.Email = emailTextBox.Text;
+                newMember.DateTime = datePicker.SelectedDate.Value.Date;
+                newMember.PostanskiBroj = GetPostalCode(citiesComboBox.SelectedItem.ToString());
+            }catch (Exception ex)
+            {
+
+            }
+            
             return newMember;
         }
         private int GetPostalCode(string name)
@@ -227,18 +264,29 @@ namespace Teretana
         }
         private void ClearAllFields()
         {
-            nameTextBox.Clear();
-            surnameTextBox.Clear();
-            datePicker.Text=String.Empty;
-            jmbgTextBox.Clear();
-            emailTextBox.Clear();
-            paidAtLbl.Content = String.Empty;
-            validUntilLbl.Content = String.Empty;
-            daysLeftLbl.Content = String.Empty; 
-            trainingsListView.ItemsSource = new List<string>();
+            ClearMemberInfoFields();
+            ClearMembershipFields();
+            ClearTrainingsFields();
             SetElementsLock(true);
         }
-
+        private void ClearMemberInfoFields()
+        {
+            nameTextBox.Clear();
+            surnameTextBox.Clear();
+            datePicker.Text = String.Empty;
+            jmbgTextBox.Clear();
+            emailTextBox.Clear();
+        }
+        private void ClearTrainingsFields()
+        {
+            trainingsListView.ItemsSource = new List<string>();
+        }
+        private void ClearMembershipFields()
+        {
+            paidAtLbl.Content = String.Empty;
+            validUntilLbl.Content = String.Empty;
+            daysLeftLbl.Content = String.Empty;
+        }
         private void SetAllFields(object sender)
         {
             int id = ((BasicMemberInfo)(sender as ListView).SelectedItem).Id;
@@ -250,6 +298,39 @@ namespace Teretana
         private void gradoviComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
+        }
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int memberId = ((BasicMemberInfo)membersListView.SelectedItem).Id;
+            string querryDeleteMembership = $"delete from clanarina where Clan_IdOsoba={memberId}";
+            string querryDeleteTrainings = $"delete from trening where Clan_IdOsoba={memberId}";
+            string querryDeleteMember = $"delete from clan where Clan_IdOsoba={memberId}";
+
+            teretanaDB.Open();
+            MySqlCommand cmd = teretanaDB.CreateCommand();
+            cmd.CommandText = querryDeleteMembership;
+            if (cmd.ExecuteNonQuery() != -1)
+            {
+                cmd.CommandText = querryDeleteTrainings;
+                if (cmd.ExecuteNonQuery() != -1)
+                {
+                    cmd.CommandText = querryDeleteMember;
+                    if(cmd.ExecuteNonQuery() == -1)
+                    {
+                        MessageBox.Show("Failed to delete the member.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Failed to delete the member.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Failed to delete the member.");
+            }
+            teretanaDB.Close();
         }
     }
 }
