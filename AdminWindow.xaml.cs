46bd1ed7938c966455644efaf33563565a3855ae
Jovo Savic <jovo.savic00@gmail.com>
2022-03-08 14:24:25 +0100

    Admin windows, load members implemented

--- AdminWindow.xaml.cs
+++ AdminWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,28 @@ namespace Teretana
     /// </summary>
     public partial class AdminWindow : Window
     {
+        MySqlConnection teretanaDB = new MySqlConnection(Config.dbConfigString);
+
         public AdminWindow()
         {
             InitializeComponent();
+            LoadEmployees();
+        }
+        private void LoadEmployees()
+        {
+            string querry = $"select IdOsoba, ime, prezime, DatumRodjenja from zaposleni join osoba on Zaposleni_IdOsoba=IdOsoba";
+            teretanaDB.Open();
+            MySqlCommand cmd = teretanaDB.CreateCommand();
+            cmd.CommandText = querry;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<EmployeeInfo> employees = new List<EmployeeInfo>();
+
+            while (reader.Read())
+            {
+                employees.Add(new EmployeeInfo(reader.GetInt32(0),reader.GetString(1), reader.GetString(2) ,reader.GetDateTime(3)));
+            }
+            employeeListView.ItemsSource = employees;
+            teretanaDB.Close();
         }
     }
 }
---
+++ EmployeeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teretana
+{
+    internal class EmployeeInfo : BasicMemberInfo
+    {
+        DateTime employmentDate;
+        double salary;
+        int contractDuration;
+        string username;
+
+        public EmployeeInfo(int id, string name, string surname, DateTime dateOfBirth):base(id,name,surname, dateOfBirth)
+        {
+
+        }
+    }
+}
